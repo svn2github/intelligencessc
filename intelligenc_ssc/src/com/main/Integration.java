2ca763fe4546b9597f373b25fe6fe2961061936f
yusudong668 <yusudong668@277a95d7-b1c5-6e24-c29b-cce9d2e2d4d5>
2011-11-04 06:28:54 +0000

    git-svn-id: https://intelligencessc.googlecode.com/svn/trunk@16 277a95d7-b1c5-6e24-c29b-cce9d2e2d4d5

--- intelligenc_ssc/src/com/main/Integration.java
+++ intelligenc_ssc/src/com/main/Integration.java
@@ -1,5 +1,38 @@
 package com.main;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.condition.StatisticsCondition;
+import com.condition.YLcondition;
+
 public class Integration {
+	
+	public static List<Integer> getResList(List<Integer> numList) {
+		
+		List<Integer> resList1 = new ArrayList<Integer>();
+		List<Integer> resList2 = new ArrayList<Integer>();
+		
+		resList1 = YLcondition.getYLHitList(numList);
+		resList2 = StatisticsCondition.getSCHitList(numList);
+		
+		if (resList1.size() != resList2.size()){
+			System.err.println("error");
+		}
+		
+		
+		return null;
+	}
+	
+	//hit的深度0-..
+	private static int getHitDepth(List<Integer> numList){
+		
+		return 0;
+	}
+	
+	private static int getNotHitDepth(List<Integer> numList){
+		
+		return 0;
+	}
 
 }
