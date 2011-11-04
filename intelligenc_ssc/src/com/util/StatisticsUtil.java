94a570e91a8fc7d39a0c6fce5beff01499366942
yusudong668 <yusudong668@277a95d7-b1c5-6e24-c29b-cce9d2e2d4d5>
2011-11-04 05:36:33 +0000

    git-svn-id: https://intelligencessc.googlecode.com/svn/trunk@12 277a95d7-b1c5-6e24-c29b-cce9d2e2d4d5

--- intelligenc_ssc/src/com/util/StatisticsUtil.java
+++ intelligenc_ssc/src/com/util/StatisticsUtil.java
@@ -1,5 +1,33 @@
 package com.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StatisticsUtil {
+	
+	
+	// 统计0-9 在一段list中出现的次数.
+	public static List<Integer> getStatistics(List<Integer> numList) {
+		List<Integer> tmpList = new ArrayList<Integer>();
+		for (int i = 0; i < 10; i++) {
+			int count = 0;
+			for (int tvalue : numList) {
+				if (tvalue == i) {
+					count ++;
+				}
+			}
+			tmpList.add(count);
+			System.out.println(i+"--"+count);
+		}
+		return tmpList;
+	}
+	
+	
+	public static void main(String[] args) {
+		List<Integer> tmpList = new ArrayList<Integer>();
+		tmpList.add(0);tmpList.add(2);tmpList.add(2);tmpList.add(2);
+		getStatistics(tmpList);
+		
+	}
 
 }
