9bb79ff8efa8c1a6802bc1949a16571f7cc35ed9
yusudong668 <yusudong668@277a95d7-b1c5-6e24-c29b-cce9d2e2d4d5>
2011-11-09 02:33:53 +0000

    git-svn-id: https://intelligencessc.googlecode.com/svn/trunk@35 277a95d7-b1c5-6e24-c29b-cce9d2e2d4d5

--- intelligenc_ssc/src/com/util/YLUtil.java
+++ intelligenc_ssc/src/com/util/YLUtil.java
@@ -23,21 +23,22 @@ public class YLUtil {
 		int tmp;
 		int tmp2;
 		numList = ListUtil.revertList(numList);
-
+        System.out.println("revert :"+numList);
 		List<Integer> ylList = new ArrayList<Integer>();
 		List<Integer> taglist = new ArrayList<Integer>();
 
-		for (int i = 0; i <= 9; i++) {
+		for (int i = 0; i <= 99; i++) {
 			ylList.add(getYLcount(i, numList));
 		}
 
-		for (int m = 0; m <= 9; m++) {
+		System.out.println("yllist"+ylList);
+		for (int m = 0; m <= 99; m++) {
 			taglist.add(m);
 		}
 
 		for (int i = 0; i < ylList.size(); i++)
 			for (int j = 0; j < ylList.size() - i - 1; ++j) {
-				if (ylList.get(j) > ylList.get(j + 1)) {
+				if (ylList.get(j) >= ylList.get(j + 1)) {
 					tmp = ylList.get(j);
 					ylList.set(j, ylList.get(j + 1));
 					ylList.set(j + 1, tmp);
@@ -54,10 +55,15 @@ public class YLUtil {
 
 	public static void main(String[] args) {
 		List<Integer> taglist = new ArrayList<Integer>();
-		for (int i = 9; i >= 0; i--) {
+		for (int i = 99; i >= 0; i--) {
 			taglist.add(i);
 		}
-		getYLcount(2, taglist);
+		taglist.set(98, 12);
+		taglist.set(95, 12);
+		System.out.println("taglist::"+taglist);
+		System.out.println("yl::"+getYLcount(12, taglist));
+		System.out.println(ylListSort(taglist));
+		
 
 	}
 
