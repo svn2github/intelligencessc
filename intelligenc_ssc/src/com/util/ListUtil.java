e64892a2e3ceb5172e425f9fcc78912911cf6484
yusudong668 <yusudong668@277a95d7-b1c5-6e24-c29b-cce9d2e2d4d5>
2011-11-15 02:45:24 +0000

    git-svn-id: https://intelligencessc.googlecode.com/svn/trunk@53 277a95d7-b1c5-6e24-c29b-cce9d2e2d4d5

--- intelligenc_ssc/src/com/util/ListUtil.java
+++ intelligenc_ssc/src/com/util/ListUtil.java
@@ -15,49 +15,91 @@ public class ListUtil {
 		}
 		return tempList;
 	}
-	
-	
-	//resList = numList - numlist2
-	public static List<Integer> subList(List<Integer> numList,List<Integer> numList2) {
-		List<Integer> resList = new ArrayList<Integer>(); 
-		for(int vv : numList){
-			if ( numList2.indexOf(vv) == -1){
+
+	// resList = numList - numlist2
+	public static List<Integer> subList(List<Integer> numList,
+			List<Integer> numList2) {
+		List<Integer> resList = new ArrayList<Integer>();
+		for (int vv : numList) {
+			if (numList2.indexOf(vv) == -1) {
 				resList.add(vv);
 			}
 		}
 
 		return resList;
 	}
-	
-	public static List<Integer> addList(List<Integer> alist,List<Integer> blist){
+
+	public static List<Integer> addList(List<Integer> alist, List<Integer> blist) {
 		List<Integer> resList = new ArrayList<Integer>();
-		for(int m :alist){
+		for (int m : alist) {
 			resList.add(m);
 		}
-		for(int n :blist){
+		for (int n : blist) {
 			resList.add(n);
 		}
-		
+
 		return resList;
-		
+
 	}
-	
+
+	// 找出最近的五个不相同的数,remove 掉
+
+	public static List<Integer> getMaxFive(List<Integer> alist) {
+		List<Integer> s1 = new ArrayList<Integer>();
+		s1.add(0);
+		s1.add(1);
+		s1.add(2);
+		s1.add(3);
+		s1.add(4);
+		s1.add(5);
+		s1.add(6);
+		s1.add(7);
+		s1.add(8);
+		s1.add(9);
+
+		List<Integer> numList = new ArrayList<Integer>();
+		List<Integer> tmpList = new ArrayList<Integer>();
+		List<Integer> resList = new ArrayList<Integer>();
+		numList = revertList(alist);
+		for (int i : numList) {
+			if (tmpList.indexOf(i) == -1 && tmpList.size() < 5) {
+				tmpList.add(i);
+			}
+
+		}
+		
+		resList = subList(s1, tmpList);
+		System.out.println(tmpList);
+		System.out.println(resList);
+		return resList;
+	}
+
 	public static void main(String[] args) {
 		List<Integer> s = new ArrayList<Integer>();
 		List<Integer> s1 = new ArrayList<Integer>();
 		s1.add(1);
-		s1.add(11);
-		s1.add(1);
-		s1.add(1);
-		
+		s1.add(2);
+		s1.add(3);
+		s1.add(4);
+		s1.add(5);
+		s1.add(6);
+		s1.add(7);
+		s1.add(2);
+		s1.add(2);
+		s1.add(2);
+		s1.add(3);
+		s1.add(4);
+
+		getMaxFive(s1);
+
 		List<Integer> s2 = new ArrayList<Integer>();
 		s2.add(1);
 		s2.add(11);
 		s2.add(3);
 		s2.add(1);
-		s = subList(s1,s2);
-		
-		System.out.println(s);
+		s = subList(s1, s2);
+
+		// System.out.println(s);
 	}
 
 }
