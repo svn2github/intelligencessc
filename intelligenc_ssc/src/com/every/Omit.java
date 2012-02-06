88da94847c9595fba3c758ee112c57f98fe9eabf
yusudong668 <yusudong668@277a95d7-b1c5-6e24-c29b-cce9d2e2d4d5>
2012-02-06 02:59:36 +0000

    git-svn-id: https://intelligencessc.googlecode.com/svn/trunk@81 277a95d7-b1c5-6e24-c29b-cce9d2e2d4d5

--- intelligenc_ssc/src/com/every/Omit.java
+++ intelligenc_ssc/src/com/every/Omit.java
@@ -3,8 +3,8 @@ package com.every;
 import java.util.List;
 
 public class Omit {
-	 private  int omitnum;
-	    private  List<Integer> resList;
+	    private  int omitnum; // 遗漏次数
+	    private  List<Integer> resList; //遗漏的列表
 		public int getOmitnum() {
 			return omitnum;
 		}
