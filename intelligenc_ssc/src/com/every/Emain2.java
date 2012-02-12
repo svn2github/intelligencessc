566f1f0ec38ad21f42960026c5ffea5f5f7a491a
yusudong668 <yusudong668@277a95d7-b1c5-6e24-c29b-cce9d2e2d4d5>
2012-02-12 04:55:08 +0000

    git-svn-id: https://intelligencessc.googlecode.com/svn/trunk@87 277a95d7-b1c5-6e24-c29b-cce9d2e2d4d5

--- intelligenc_ssc/src/com/every/Emain2.java
+++ intelligenc_ssc/src/com/every/Emain2.java
@@ -3,6 +3,7 @@ package com.every;
 import java.util.ArrayList;
 import java.util.List;
 
+//7 码 滚钱
 public class Emain2 {
 	public static void main(String[] args) {
 		List<Integer> beishu = new ArrayList<Integer>();
@@ -10,68 +11,40 @@ public class Emain2 {
 		List<Integer> tmlist = new ArrayList<Integer>();
 
 		List<Integer> nums0 = new ArrayList<Integer>();
-		List<Integer> nums1 = new ArrayList<Integer>();
-		List<Integer> nums2 = new ArrayList<Integer>();
-		List<Integer> nums3 = new ArrayList<Integer>();
-		List<Integer> nums4 = new ArrayList<Integer>();
 		List<Integer> reses = new ArrayList<Integer>();
 
 		nums0 = GetSrc.readFileSingle(0);
-		nums1 = GetSrc.readFileSingle(1);
-		nums2 = GetSrc.readFileSingle(2);
-		nums3 = GetSrc.readFileSingle(3);
-		nums4 = GetSrc.readFileSingle(4);
 		List<Omit> os = new ArrayList<Omit>();
 		int len = nums0.size();
-		int cc = 0;
 		for (int i = 30; i < len - 1; i++) {
 
 			Omit o0 = new Omit();
-			Omit o1 = new Omit();
-			Omit o2 = new Omit();
-			Omit o3 = new Omit();
-			Omit o4 = new Omit();
 
+			//七码
 			o0 = OmitUtil.getOmitNum(nums0.subList(i - 26, i - 1), 3);
-			o1 = OmitUtil.getOmitNum(nums1.subList(i - 26, i - 1), 3);
-			o2 = OmitUtil.getOmitNum(nums2.subList(i - 26, i - 1), 3);
-			o3 = OmitUtil.getOmitNum(nums3.subList(i - 26, i - 1), 3);
-			o4 = OmitUtil.getOmitNum(nums4.subList(i - 26, i - 1), 3);
 
 			int a0 = o0.getOmitnum();
-			int a1 = o1.getOmitnum();
-			int a2 = o2.getOmitnum();
-			int a3 = o3.getOmitnum();
-			int a4 = o4.getOmitnum();
 
-			List<Integer> n0 = new ArrayList<Integer>();
-			List<Integer> n1 = new ArrayList<Integer>();
-			List<Integer> n2 = new ArrayList<Integer>();
-			List<Integer> n3 = new ArrayList<Integer>();
-			List<Integer> n4 = new ArrayList<Integer>();
 
-			n0 = o0.getResList();
-			n1 = o1.getResList();
-			n2 = o2.getResList();
-			n3 = o3.getResList();
-			n4 = o4.getResList();
-
-			int as[] = { a0, a1, a2, a3, a4 };
-			// System.out.println(as[0]+","+as[1]+","+as[2]+","+as[3]+","+as[4]);
-//			int a = getmax(as);
 			if (a0 > 4) {
-				os.add(o0);
-				tmlist.add(nums0.get(i));
+				os.add(o0);// 遗漏数,遗漏表
+				tmlist.add(nums0.get(i));//奖号
 			}
 
 
 		}
 		System.out.println("os size:" +os.size() + "==all size:" + len);
 
+		
+		
+		
+		//遗漏表对应的中奖状况
 		for (int j = 0; j < os.size(); j++) {
+			
 			List<Integer> j1 = new ArrayList<Integer>();
-			j1 = os.get(j).getResList();
-			int tm = tmlist.get(j);
+			j1 = os.get(j).getResList();// 遗漏的列表
+			
+			int tm = tmlist.get(j); // 奖号
 			if (checksuit(tm, j1)) {
 				reses.add(1);
 //				System.out.println(tm+"==" + j1+"==="+1);
@@ -81,23 +54,92 @@ public class Emain2 {
 			}
 
 		}
+		
+		System.out.println("=================================");
 		//reses : 结果表
 		//os : 遗漏
 		
 		int len1 = reses.size();
 		int tag = 0;
-		while(tag < len1){
+		while(tag < len1-30){
 			int cost = 0;
 			int pro = 0;
 			int tt = 0;
-			int re = reses.get(tag);
+			int res = reses.get(tag);
+			//遗漏表
 			List<Integer> la = new ArrayList<Integer>();
 			la = os.get(tag).getResList();
 			
+			int cn = 0;
+			while(true){
+				tag++;
+				List<Integer> tmp = new ArrayList<Integer>();
+				tmp = os.get(tag).getResList();
+				if(checksame(la,tmp)){
+					cn ++;
+				}else{
+					break;
+				}
+				
+			}
+			
+			System.out.println(cn);
+			
+			
 			
 		}
-//		System.out.println(cost+"==="+pro);
 	}
+	
+	
+	
+	
+	
+	//=======================================================
+	
+	public static int getDoubleHit(List<Integer> yllist,List<Integer> numlist){
+		
+		int count=0;
+		for(int i=0;i<numlist.size()-1;i++){
+			count++;
+			if(checksuit(numlist.get(i),yllist) && checksuit(numlist.get(i+1),yllist)){
+				break;
+			}
+			
+		}
+		
+		
+		
+		return count;
+	}
+	
+	
+	
+	
+	public static int  getB(int cn){
+		
+		List<Integer> s = new ArrayList<Integer>();
+		s.add(1);
+		s.add(2);
+		s.add(2);
+		s.add(7);
+		s.add(7);
+		s.add(26);
+		s.add(26);
+		s.add(96);
+		s.add(96);
+		s.add(354);
+		s.add(354);
+		s.add(1300);
+		s.add(1300);
+		
+		
+		return cn;
+		
+	}
+	
+	
+	
+	
 	public static boolean checksuit(int a, List<Integer> numlist) {
 
 		boolean tag = false;
@@ -110,6 +152,9 @@ public class Emain2 {
 		return tag;
 	}
 	
+	
+	
+	//列表相等
 	public static boolean checksame(List<Integer> l1,List<Integer> l2){
 		int len = l1.size();
 		boolean tag = true;
