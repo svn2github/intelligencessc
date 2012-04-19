b39edf5b3fb431933c5bddd6d25a7091adb52e1b
yusudong668 <yusudong668@277a95d7-b1c5-6e24-c29b-cce9d2e2d4d5>
2012-04-19 03:13:42 +0000

    git-svn-id: https://intelligencessc.googlecode.com/svn/trunk@103 277a95d7-b1c5-6e24-c29b-cce9d2e2d4d5

--- intelligenc_ssc/src/translocation/Main.java
+++ intelligenc_ssc/src/translocation/Main.java
@@ -8,6 +8,9 @@ import com.util.SrcUtil;
 
 public class Main {
 
+	public static final int QS = 30;
+	public static final double PF = 1.93;
+
 	public static void main(String[] args) {
 		// double y = 9.999999;
 		//
@@ -24,102 +27,129 @@ public class Main {
 
 		List<Integer> s1 = new ArrayList<Integer>();
 		List<Integer> s2 = new ArrayList<Integer>();
+		
+		List<Integer> sa = new ArrayList<Integer>();
+		List<Integer> sb = new ArrayList<Integer>();
 
+		s1.add(0);
 		s1.add(1);
 		s1.add(2);
 		s1.add(3);
-		s1.add(5);
-		s1.add(7);
-		s2.add(0);
-		s2.add(4);
+		s1.add(4);
+		s2.add(5);
 		s2.add(6);
+		s2.add(7);
 		s2.add(8);
 		s2.add(9);
+		
+		sa.add(0);
+		sa.add(2);
+		sa.add(3);
+		sa.add(5);
+		sa.add(7);
+		sb.add(1);
+		sb.add(4);
+		sb.add(6);
+		sb.add(8);
+		sb.add(9);
+		
 
 		for (int i = 0; i < list4.size(); i++) {
 			resa.add(checksuit(list4.get(i), s1));
-			resb.add(checksuit(list4.get(i), s2));
+			resb.add(checksuit(list4.get(i), sa));
 		}
 
-
+		List<Double> res1 = new ArrayList<Double>();
+		List<Double> res2 = new ArrayList<Double>();
 		List<Double> res = new ArrayList<Double>();
-		int count = 0;
+		
+		res1 = getRes(resa);
+		res2 = getRes(resb);
+		
+		System.out.println(res1.size());
+		System.out.println(res2.size());
+		
+		for (int i = 0 ;i<res1.size();i++){
+			res.add(res1.get(i)+res2.get(i));
+		}
+		System.out.println(res);
 
-		int tag = 120;
-		for (int j = 0; j < resa.size(); j += tag) {
-			count++;
+		List<Integer> fi = new ArrayList<Integer>();
+
+		double li = 0.0;
+		for (int l = 0; l < res.size(); l++) {
+			li += res.get(l);
+			if (res.get(l) > 0) {
+				fi.add(1);
+			} else {
+				fi.add(0);
+			}
+		}
+
+		List<Integer> hi = new ArrayList<Integer>();
+		for (int mm = 20; mm < fi.size(); mm++) {
+			List<Integer> templ = new ArrayList<Integer>();
+			templ = fi.subList(mm - 20, mm - 1);
+			hi.add(getNotHitDepth(templ));
+		}
+
+		int max = 0;
+		for (int m : hi) {
+			if (m > max) {
+				max = m;
+			}
+		}
+		System.out.println("==:" + max);
+		System.out.println("pro=:" + getOneS(li));
+		System.out.println(hi);
+
+	}
+
+	public static List<Double> getRes(List<Integer> resa) {
+		List<Double> res = new ArrayList<Double>();
+
+		for (int j = 0; j < resa.size(); j += QS) {
 			double cost = 2.0;
-			double pro = 1.9;
-			for (int m = j + 1; m < j + tag; m++) {
-				if(m > resa.size()-1 ){
-					break;
-				}
+			double pro = PF;
+			for (int m = j + 1; m < j + QS; m++) {
 				int a0 = resa.get(m - 1);
 				int a1 = resa.get(m);
 
 				if (a0 == 1) {
 
 					if (a1 == 1) {
-						pro = pro * 1.9;
+						pro = pro * PF;
 						pro = getOneS(pro);
 						cost += 1.0;
 					} else if (a1 == 0) {
-						pro = 1.9;
+						pro = PF;
 						cost += 1.0;
 					}
 
 				} else if (a0 == 0) {
 
 					if (a1 == 1) {
-						pro = 1.9;
+						pro = PF;
 						cost += 1.0;
 					} else if (a1 == 0) {
 						cost += 1.0;
-						pro = pro * 1.9;
+						pro = pro * PF;
 						pro = getOneS(pro);
-						
+
 					}
 
 				}
-				if (pro / cost > 2 && pro-cost > 13) {
-					res.add(pro - cost);
+				if (m > resa.size() -2) {
+					break;
+				}
+				if (pro - cost > 10) {
 					break;
 				}
 
 			}
 			res.add(pro - cost);
 		}
-		
-		System.out.println(res);
-		List<Integer> fi = new ArrayList<Integer>();
-
-		double li = 0.0;
-		for (int l = 0; l < res.size(); l++) {
-			li += res.get(l);
-			if (res.get(l) > 0 ){
-				fi.add(1);
-			}else {
-				fi.add(0);
-			}
-		}
-		
-		List<Integer> hi = new ArrayList<Integer>();
-		for(int mm=20;mm<fi.size();mm++){
-			List<Integer> templ = new ArrayList<Integer>();
-			templ=fi.subList(mm-20, mm-1);
-			hi.add(getNotHitDepth(templ));
-		}
-		
-		int max = 0;
-		for(int m : hi){
-			if(m > max){
-				max = m;
-			}
-		}
-		System.out.println("==:"+max);
-		System.out.println("pro=:"+getOneS(li));
-		System.out.println("count=:"+count);
-		System.out.println(hi);
+		return res;
 
 	}
 
@@ -159,12 +189,11 @@ public class Main {
 	}
 
 	public static int getNotHitDepth(List<Integer> numList) {
-		numList = ListUtil.revertList(numList);
 		int rest = 0;
 		int count = 0;
 		for (int i = 0; i < numList.size(); i++) {
 			rest += numList.get(i);
-			
+
 			if (rest > 0) {
 				break;
 			}
@@ -173,5 +202,4 @@ public class Main {
 		return count;
 	}
 
-	
 }
