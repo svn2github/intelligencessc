2813020f36ef41d284385535b5a7366662be4fbb
yusudong668 <yusudong668@277a95d7-b1c5-6e24-c29b-cce9d2e2d4d5>
2013-07-30 07:01:20 +0000

    git-svn-id: https://intelligencessc.googlecode.com/svn/trunk@119 277a95d7-b1c5-6e24-c29b-cce9d2e2d4d5

--- intelligenc_ssc/src/hou2/Hou2Main.java
+++ intelligenc_ssc/src/hou2/Hou2Main.java
@@ -1,8 +1,34 @@
 package hou2;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.util.SrcUtil;
+
 public class Hou2Main {
 	
+	// 按照时间升序
+	// list 0 - 2011-10-01 -123
+	// list 1 - 2011-10-02 -112 
 	public static void main(String[] args) {
+		
+		List<Integer> listn = new ArrayList<Integer>();//
+		
+		List<Integer> list3 = new ArrayList<Integer>();// 十
+		List<Integer> list4 = new ArrayList<Integer>();// 个
+
+		list3 = SrcUtil.readFileSingle("src/cqssc.txt", 3);
+		list4 = SrcUtil.readFileSingle("src/cqssc.txt", 4);
+		
+		
+		List<Integer> tslist0 = new ArrayList<Integer>();
+		List<Integer> tslist1 = new ArrayList<Integer>();
+		List<Integer> tslist2 = new ArrayList<Integer>();
+		List<Integer> tslist3 = new ArrayList<Integer>();
+		List<Integer> tslist4 = new ArrayList<Integer>();
+		
+		
+		
 		
 	}
 
